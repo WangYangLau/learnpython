47aab9d2c591a1d46a99beb89e9f945c7dde3c49
WangYang <d13532641570@sina.com>
2018-01-31 17:33:49 +0800

    add func_use&Practice

---
+++ Practice_6.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+
+n1 = 255
+n2 = 1000
+m1 = hex(n1)
+m2 = hex(n2)
+print("origin n1 : %d"%n1)
+print("origin n2 : %d"%n2)
+print("after hex m1 : %s"%m1)
+print("after hex m2 : %s"%m2)
---
+++ func_use.py
@@ -0,0 +1,8 @@
+# -*- coding:utf-8 -*-
+
+num=-1
+print("origin num : %d"%num)
+print("after abs : %d"%abs(num))
+print("after max : %d"%max(1,-1,2,5,3,-3))
+flo=1.6
+print("after int : %d"%int(flo))
